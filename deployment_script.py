bb7912015af60d44144cdcb7ca5a0aeedf8223cf
Rahul Rakesh <rahulrakesh@gmail.com>
2018-08-29 12:18:35 +0530

    edited index

--- deployment_script.py
+++ deployment_script.py
@@ -8,17 +8,30 @@ import mimetypes
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:ap-south-1:102885605453:DeployFolioTopic')
+    location = {
+        "bucketName": 'build.rahulrakesh.me',
+        "objectKey": 'portfoliobuild.zip'
+    }
 
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == 'MyAppBuild':
+                    location = artifact['location']['s3Location']
+
+        print("building portfolio from " + str(location))
+
         s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
         # s3 = boto3.resource('s3')
         #initialize code and production buckets
         folio_bucket = s3.Bucket('rahulrakesh.me')
-        build_bucket = s3.Bucket('build.rahulrakesh.me')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         # create in-memory bytes io file and import the file
         portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         # upload the contents of the file to production s3 bucket
         with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -28,7 +41,9 @@ def lambda_handler(event, context):
                 folio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         topic.publish(Subject='Deployment Complete', Message='Folio deployed successfully')
-
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
     except:
         topic.publish(Subject='Deployment Failed', Message='Folio could not be deployed')
         raise
