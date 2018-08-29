cd7adaf77967d7245fd9cfebe6d048887f7e475d
Rahul Rakesh <rahulrakesh@gmail.com>
2018-08-29 10:04:44 +0530

    updated lambda handler function for lambda

--- deployment_script.py
+++ deployment_script.py
@@ -1,23 +1,31 @@
+import json
 import boto3
 from botocore.client import Config
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
-# s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-#initialize code and production buckets
-folio_bucket = s3.Bucket('rahulrakesh.me')
-build_bucket = s3.Bucket('build.rahulrakesh.me')
+    s3 = boto3.resource('s3', config = Config(signature_version='s3v4'))
+    # s3 = boto3.resource('s3')
 
-# create in-memory bytes io file and import the file
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    #initialize code and production buckets
+    folio_bucket = s3.Bucket('rahulrakesh.me')
+    build_bucket = s3.Bucket('build.rahulrakesh.me')
 
-# upload the contents of the file to production s3 bucket
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        folio_bucket.upload_fileobj(obj, nm, ExtraArgs = {'ContentType': mimetypes.guess_type(nm)[0]})
-        folio_bucket.Object(nm).Acl().put(ACL='public-read')
+    # create in-memory bytes io file and import the file
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    # upload the contents of the file to production s3 bucket
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            folio_bucket.upload_fileobj(obj, nm, ExtraArgs = {'ContentType': mimetypes.guess_type(nm)[0]})
+            folio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps('Hello from Lambda!')
+    }
